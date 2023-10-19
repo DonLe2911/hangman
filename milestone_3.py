57185bf8774b97ad1130006f2a4d7a328fefa234
DonLe2911 <don.le@hotmail.co.uk>
2023-10-19 13:28:16 +0100

    moved code into funcitons

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,27 @@
-while True:
-    guess = input("Guess a letter ")
-    if len(guess) == 1 and guess.isalpha():
-        break
+import random
+
+word_list = ["kiwi", "apple", "durian", "dragonfruit", "pear"]
+
+random_word_from_list = random.choice(word_list)
+
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in random_word_from_list:
+        print(f"Good guess! {guess} is in the word")
     else:
-        print("Invalid letter, please enter a single alphabetical character")
+        print(f"Sorry, {guess} is not in the word")
+
+
+def ask_for_input():
+       
+    while True:
+        guess = input("Guess a letter ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter, please enter a single alphabetical character")
+    
+    check_guess(guess)
+    
+ask_for_input()
