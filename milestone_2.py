82f37b8b830bd11f93b8405d4f3c0b269af10e36
DonLe2911 <don.le@hotmail.co.uk>
2023-10-19 11:30:16 +0100

    dfjokghsrdgjhn

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,12 @@
 import random
 
-word_list = ["kiwi", "apple", "durian", "dragonfruit", "pear"]#
+word_list = ["kiwi", "apple", "durian", "dragonfruit", "pear"]
 
-word = random.choice(word_list)
+random_word_from_list = random.choice(word_list)
 
-guess = input("guess a letter ")
+player_guess = input("guess a letter ")
 
-if len(guess) == 1 and guess.isalpha():
+if len(guess) == 1 and player_guess.isalpha():
     print("Valid guess")
 else:
     print("Ooops! That is not a valid guess")
